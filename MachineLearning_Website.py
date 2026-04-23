781204c90fef7b0957df345f6e32bf74c3f45625
OmerC16 <cohen.omer2009@gmail.com>
2026-04-23 09:44:29 +0300

    Update MachineLearning_Website.py

--- MachineLearning_Website.py
+++ MachineLearning_Website.py
@@ -3,7 +3,7 @@ import kagglehub
 import pandas as pd
 import os
 
-path = kagglehub.dataset_download("mexwell/astronauts-in-space-missions")
+path = kagglehub.dataset_download("fedesoriano/stroke-prediction-dataset")
 files = os.listdir(path)
 file_path = os.path.join(path, files[0])
 df = pd.read_csv(file_path)
