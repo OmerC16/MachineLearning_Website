a6307fe1e18f9166962973b7aeb4b7665a093369
OmerC16 <cohen.omer2009@gmail.com>
2026-04-23 10:22:34 +0300

    Update MachineLearning_Website.py

--- MachineLearning_Website.py
+++ MachineLearning_Website.py
@@ -2,8 +2,8 @@ import streamlit as st
 import kagglehub
 import pandas as pd
 import os
-import DecisionTreeClassifier, plot_tree
-import preprocessing
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+from sklearn import preprocessing
 
 path = kagglehub.dataset_download("fedesoriano/stroke-prediction-dataset")
 files = os.listdir(path)
