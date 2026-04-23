0f6ddec04f46358864001bc989f2976b62ab367a
OmerC16 <cohen.omer2009@gmail.com>
2026-04-23 10:51:56 +0300

    Update MachineLearning_Website.py

--- MachineLearning_Website.py
+++ MachineLearning_Website.py
@@ -1,13 +1,13 @@
-#import streamlit as st
-#import kagglehub
-#import pandas as pd
-#import os
+import streamlit as st
+import kagglehub
+import pandas as pd
+import os
 
-"""path = kagglehub.dataset_download("fedesoriano/stroke-prediction-dataset")
+path = kagglehub.dataset_download("fedesoriano/stroke-prediction-dataset")
 files = os.listdir(path)
 file_path = os.path.join(path, files[0])
 df = pd.read_csv(file_path)
-st.table(df.head())"""
+st.table(df.head())
 
 """le = sklearn.preprocessing.LableEncoder()
 df["age"] = le.fir_transform(df["age"])
