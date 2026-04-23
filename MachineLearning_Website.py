81cfe7f11f338dd6683fd0850d597bd0ef5b029e
OmerC16 <cohen.omer2009@gmail.com>
2026-04-23 09:30:54 +0300

    Update MachineLearning_Website.py

--- MachineLearning_Website.py
+++ MachineLearning_Website.py
@@ -1,2 +1,11 @@
-import streamlit
-streamlit.write("האתר שלי")
+import streamlit as st
+import kagglehub
+import pandas as pd
+import os
+
+path = kagglehub.dataset_download("mexwell/astronauts-in-space-missions")
+files = os.listdir(path)
+file_path = os.path.join(path, files[0])
+df = pd.read_csv(file_path)
+
+st.table(df.head())
