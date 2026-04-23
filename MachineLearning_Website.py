9be5cda5a130a262f42cecdb1bcef3fe0b60ff50
OmerC16 <cohen.omer2009@gmail.com>
2026-04-23 10:37:42 +0300

    Update MachineLearning_Website.py

--- MachineLearning_Website.py
+++ MachineLearning_Website.py
@@ -9,7 +9,7 @@ file_path = os.path.join(path, files[0])
 df = pd.read_csv(file_path)
 st.table(df.head())
 
-#le = sklearn.preprocessing.LableEncoder()
+"""le = sklearn.preprocessing.LableEncoder()
 df["age"] = le.fir_transform(df["age"])
 
 features = ["age", "gender", "hyper_tension", "heart_diseas", "ever_married", "work_type", "Residence_type", "avg_glucose_level", "bmi", "smoking_status"]
@@ -19,4 +19,4 @@ y = df["stroke"]
 #model = sklearn.tree.DecisionTreeClassifier(max_depth=9)
 #model.fit(z, y)
 
-#st.write(model.score(x, y))
+st.write(model.score(x, y))"""
