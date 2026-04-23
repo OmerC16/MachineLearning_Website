b066f6c64839502c6caa37edac4a5ebcda1a5471
OmerC16 <cohen.omer2009@gmail.com>
2026-04-23 10:16:41 +0300

    Update MachineLearning_Website.py

--- MachineLearning_Website.py
+++ MachineLearning_Website.py
@@ -2,10 +2,22 @@ import streamlit as st
 import kagglehub
 import pandas as pd
 import os
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+from sklearn import preprocessing
 
 path = kagglehub.dataset_download("fedesoriano/stroke-prediction-dataset")
 files = os.listdir(path)
 file_path = os.path.join(path, files[0])
 df = pd.read_csv(file_path)
 
-st.table(df.head())
+le = preprocessing.LableEncoder()
+df["age"] = le.fir_transform(df["age"])
+
+features = ["age", "gender", "hyper_tension", "heart_diseas", "ever_married", "work_type", "Residence_type", "avg_glucose_level", "bmi", "smoking_status"]
+x = df[features]
+y = df["stroke"]
+
+model = DecisionTreeClassifier(max_depth=9)
+model.fit(z, y)
+
+st.write(model.score(x, y))
