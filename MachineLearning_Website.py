d9402ab6d63a1a4917e7052dd04aed6069b73bd8
OmerC16 <cohen.omer2009@gmail.com>
2026-05-28 09:42:16 +0300

    Update MachineLearning_Website.py

--- MachineLearning_Website.py
+++ MachineLearning_Website.py
@@ -1,2 +1,10 @@
+import streamlit as st
+import kagglehub
 import pandas as pd
 import os
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("aiexplorer77/academic-performance-prediction")
+
+print("Path to dataset files:", path)
