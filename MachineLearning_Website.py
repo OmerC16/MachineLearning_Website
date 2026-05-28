cdd6f28753ee9a38e71aa748cfbdac2313cf9032
OmerC16 <cohen.omer2009@gmail.com>
2026-05-28 09:30:26 +0300

    Update MachineLearning_Website.py

--- MachineLearning_Website.py
+++ MachineLearning_Website.py
@@ -5,7 +5,7 @@ import streamlit as st
 
 path = kagglehub.dataset_download("aiexplorer77/academic-performance-prediction")
 
-df = pd.read_csv(os.path.join(path, "student_performance_dataset.csv"))
+df = pd.read_csv(path)
 df = df["attendance_percentage", "final_exam_score"]
 
 st.table(df)
