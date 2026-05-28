ef53d1ceb872d48593236f94220fddfa22e94e86
OmerC16 <cohen.omer2009@gmail.com>
2026-05-28 09:23:21 +0300

    Update MachineLearning_Website.py

--- MachineLearning_Website.py
+++ MachineLearning_Website.py
@@ -3,9 +3,7 @@ import pandas as pd
 import os
 import streamlit
 
-path = kagglehub.dataset_download(
-  "adityadesai13/used-car-dataset-ford-and-mercedes"
-)
+path = kagglehub.dataset_download("aiexplorer77/academic-performance-prediction")
 
-df = pd.read_csv(os.path.join(path, "ford_csv"))
-df = df["mileage", "price"]
+df = pd.read_csv(os.path.join(path, "student_performance_dataset_csv"))
+df = df["attendance_percentage", "final_exam_score"]
