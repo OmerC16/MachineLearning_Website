f5b88af64b2dcda8da777f22b1762cf6de11c757
OmerC16 <cohen.omer2009@gmail.com>
2026-09-09 09:19:24 +0300

    Update MachineLearning_Website.py

--- MachineLearning_Website.py
+++ MachineLearning_Website.py
@@ -1,1 +1,9 @@
 import streamlit as st
+import kagglehub
+import pandas as pd
+
+path = kagglehub.dataset_download("shubhambathwal/flight-price-prediction")3
+data = pd.read_csv(path)
+data = data["duration", "price"]
+
+st.table(data)
