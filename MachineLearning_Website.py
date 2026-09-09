35f53cbfbf31a638a237994a3e9b99e6dfba96de
OmerC16 <cohen.omer2009@gmail.com>
2026-09-09 09:20:33 +0300

    Update MachineLearning_Website.py

--- MachineLearning_Website.py
+++ MachineLearning_Website.py
@@ -2,7 +2,7 @@ import streamlit as st
 import kagglehub
 import pandas as pd
 
-path = kagglehub.dataset_download("shubhambathwal/flight-price-prediction")3
+path = kagglehub.dataset_download("shubhambathwal/flight-price-prediction")
 data = pd.read_csv(path)
 data = data["duration", "price"]
 
